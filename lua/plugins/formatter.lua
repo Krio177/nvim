29510bd78e3a07f8dea169d2c0ba49f63d8d3cb6
Teigler Tibor <ttibi17@gmail.com>
2025-05-21 19:38:23 +0200

    formatter

---
+++ lua/plugins/formatter.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("conform").setup {
+        formatters_by_ft = {
+          html = { "prettier" },
+          lua = { "stylua" },
+          javascript = { "prettier" },
+          go = { "goimports", "gofmt", stop_after_first = true },
+          ["*"] = { "trim_whitespace" },
+        },
+        format_on_save = {
+          lsp_fallback = true,
+          timeout_ms = 500,
+        },
+      }
+
+      -- Manuális formázás hotkey
+      vim.keymap.set({ "n", "v" }, "<leader>cf", function()
+        require("conform").format { async = true, lsp_fallback = true }
+      end, { desc = "[C]ode [F]ormat" })
+    end,
+  },
+}
